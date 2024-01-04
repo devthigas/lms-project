50373e29b1fb3d5db3241da08bdd13f018b9c171
thigas <thi.deusefiel@gmail.com>
2024-01-03 21:19:57 -0300

    adicionando Middleware no servidor

--- server/app.ts
+++ server/app.ts
@@ -1,3 +1,30 @@
-import express from 'express'
+require('dotenv').config();
+import express, { Request, Response, NextFunction } from 'express';
+export const app = express();
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
-export const app = express()
+app.use(express.json({limit: "50mb"}));
+app.use(cookieParser());
+
+app.use(cors({
+    origin: process.env.ORIGIN,
+}));
+
+
+// testing route 
+
+app.get('/test', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({
+        sucess: true,
+        message: "server is up and running"
+    });
+}); 
+
+app.get('*', (req: Request, res: Response, next: NextFunction) => {
+   const error = new Error(` Route ${req.originalUrl} not found`) as any ;
+   error.statusCode = 404;
+   next(error);
+   
+});
+
